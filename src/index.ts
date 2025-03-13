6b13be4cf8700aa121124d0ca1624d454e76a0a1
worksofliam <mrliamallan@live.co.uk>
2025-03-13 12:45:07 -0400

    Correctly export
    Signed-off-by: worksofliam <mrliamallan@live.co.uk>

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log(`hi`);
+const isCli = process.argv.length >= 2 && (process.argv[1].endsWith(`so`) || process.argv[1].endsWith(`index.js`));
+
+if (isCli || process.env.VSCODE_INSPECTOR_OPTIONS) {
+	main();
+}
+
+async function main() {
+  console.log(`hi!`);
+}
+
+export * from "./types";
+export { LogicGoose } from "./logicgoose";
