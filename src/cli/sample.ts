d59bcb3bdf5a1a10222ac323f03db7846a98035a
worksofliam <mrliamallan@live.co.uk>
2025-03-13 14:38:01 -0400

    Samples for everything
    Signed-off-by: worksofliam <mrliamallan@live.co.uk>

--- src/cli/sample.ts
+++ src/cli/sample.ts
@@ -1,15 +1,16 @@
 import { LGFile } from "../types";
 
 export const SAMPLE_LG_FILE: LGFile = {
+  generateIn: "src",
   generateCaller: true,
   generateTypes: true,
-  generateSql: false,
-  generateRpgle: false,
+  generateSql: true,
+  generateRpgle: true,
   callers: [
     {
-      programName: `ABCD`,
+      programName: `SIMPLE`,
       programLibrary: `LIB`,
-      procedureName: `test`,
+      procedureName: `simple`,
       procedureLibrary: `LIB`,
       bufferIn: [
         { name: `name`, length: 10 },
@@ -20,6 +21,124 @@ export const SAMPLE_LG_FILE: LGFile = {
         { name: `coolness`, length: 10, decimals: 0 },
         { name: `result`, length: 10 }
       ],
+    },
+    {
+      programName: `BOOLEAN`,
+      programLibrary: `LIB`,
+      procedureName: `boolean`,
+      procedureLibrary: `LIB`,
+      bufferIn: [
+        {
+          name: "name",
+          length: 10
+        },
+        {
+          name: "number",
+          length: 5,
+          decimals: 2
+        },
+        {
+          name: "abool",
+          length: 1
+        }
+      ],
+      bufferOut: [
+        { name: `name`, length: 10 },
+        { name: `coolness`, length: 10, decimals: 0 },
+        { name: `resbool`, length: 1 }
+      ],
+    },
+    {
+      programName: `STRUCT`,
+      programLibrary: `LIB`,
+      procedureName: `struct`,
+      procedureLibrary: `LIB`,
+      bufferIn: [
+        {
+          "name": "name",
+          "length": 10
+        },
+        {
+          "name": "number",
+          "length": 5,
+          "decimals": 2
+        },
+        {
+          "name": "abool",
+          "length": 1
+        },
+        {
+          "name": "subf",
+          "like": [
+            {
+              "name": "subfA",
+              "length": 1
+            },
+            {
+              "name": "subfB",
+              "length": 4
+            },
+            {
+              "name": "subfC",
+              "length": 5,
+              "decimals": 2
+            },
+          ]
+        }
+      ],
+      bufferOut: [
+        {
+          "name": "name",
+          "length": 10
+        },
+        {
+          "name": "number",
+          "length": 5,
+          "decimals": 2
+        },
+        {
+          "name": "abool",
+          "length": 1
+        },
+        {
+          "name": "subf",
+          "like": [
+            {
+              "name": "subfA",
+              "length": 1
+            },
+            {
+              "name": "subfB",
+              "length": 4
+            },
+            {
+              "name": "subfC",
+              "length": 5,
+              "decimals": 2
+            },
+          ]
+        }
+      ]
+    },
+    {
+      programName: `ARRAY`,
+      programLibrary: `LIB`,
+      procedureName: `array`,
+      procedureLibrary: `LIB`,
+      bufferIn: [
+        { name: `customerId`, length: 10, decimals: 0}
+      ],
+      bufferOut: [
+        { name: `customerId`, length: 10, decimals: 0},
+        {
+          name: "orders",
+          like: [
+            { name: "id", length: 10, decimals: 0 },
+            { name: "easyName", length: 50 },
+          ],
+          dim: 100
+        }
+      ]
     }
   ]
 }
